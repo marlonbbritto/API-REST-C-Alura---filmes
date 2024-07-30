cade13425052eb86bb07ff293b174cc6b4fb76e2
marlonbbritto <marlonbbritto@gmail.com>
2024-07-29 22:07:14 -0300

    Incluindo a conexão com banco e persistencia de dados

--- FilmesApi/Controllers/FilmeControleer.cs
+++ FilmesApi/Controllers/FilmeControleer.cs
@@ -1,3 +1,4 @@
+using FilmesApi.Data;
 using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.TagHelpers.Cache;
@@ -9,26 +10,30 @@ namespace FilmesApi.Controllers;
 
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new List<Filme>();
-    private static int id=0;
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
     public IActionResult AdicionarFilme([FromBody] Filme filme)
     {
-        filme.Id = id++;
-        filmes.Add(filme);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
         return CreatedAtAction(nameof(RecuperarFilmePorId), new {id=filme.Id}, filme);        
     }
 
     [HttpGet]
     public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, int take= 50 )
     {
-        return filmes.Skip(skip).Take(take);
+        return _context.Filmes.Skip(skip).Take(take);
     }
     [HttpGet("{id}")]
     public IActionResult RecuperarFilmePorId(int id)
     {
-        var filme = filmes.FirstOrDefault(filme=>filme.Id==id);
+        var filme = _context.Filmes.FirstOrDefault(filme=>filme.Id==id);
         if (filme==null) return NotFound();
         return Ok(filme);
     }
