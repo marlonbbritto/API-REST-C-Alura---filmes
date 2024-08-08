7d2ce660cfff1b7ee3557a52d210f2a01f5af691
marlonbbritto <marlonbbritto@gmail.com>
2024-08-07 21:00:00 -0300

    Criando uma nova classe para Cinema

---
+++ FilmesApi/Profiles/CinemaProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FilmesApi.Data.Dtos;
+using FilmesApi.Models;
+
+namespace FilmesApi.Profiles
+{
+    public class CinemaProfile : Profile
+    {
+        protected CinemaProfile()
+        {
+            CreateMap<CreateCinemaDto, Cinema>();
+            CreateMap<Cinema, ReadCinemaDto>();
+            CreateMap<UpdateCinemaDto, Cinema>();
+
+        }
+    }
+}
--- FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
+++ FilmesApi/obj/Debug/net8.0/FilmesApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747d1482e3ab24bdb24dec64f24ebbe518c58c97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bc985c5ac21b033c585bc6e765fbfb3ad81559e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmesApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
